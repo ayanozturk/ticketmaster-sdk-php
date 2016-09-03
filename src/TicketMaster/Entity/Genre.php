4d63de2062cae65b0a3fd6daca56a49e0f635fb3
Saphyel <Saphyel@gmail.com>
2016-09-03 14:03:04 +0100

    Add Genre

---
+++ src/TicketMaster/Entity/Genre.php
@@ -0,0 +1,52 @@
+<?php
+namespace TicketMaster\Entity;
+
+
+class Genre
+{
+    /**
+     * @var string
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Genre
+     */
+    public function setId(string $id): Genre
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Genre
+     */
+    public function setName(string $name): Genre
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+}
