e3cea0d11daff73c1452534e75d75bc56a166739
Saphyel <Saphyel@gmail.com>
2016-09-03 12:26:53 +0100

    Add Category fields

--- src/TicketMaster/Entity/Category.php
+++ src/TicketMaster/Entity/Category.php
@@ -7,5 +7,91 @@ namespace TicketMaster\Entity;
  */
 class Category
 {
+  /**
+   * @var string
+   */
+  private $id;
+  /**
+   * @var string
+   */
+  private $name;
+  /**
+   * @var string
+   */
+  private $locale;
+  /**
+   * @var int
+   */
+  private $level;
+
+  /**
+   * @return string
+   */
+  public function getId(): string
+  {
+    return $this->id;
+  }
+
+  /**
+   * @param string $id
+   * @return Category
+   */
+  public function setId(string $id) {
+    $this->id = $id;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  /**
+   * @param string $name
+   * @return Category
+   */
+  public function setName(string $name)
+  {
+    $this->name = $name;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getLocale(): string
+  {
+    return $this->locale;
+  }
+
+  /**
+   * @param string $locale
+   * @return Category
+   */
+  public function setLocale(string $locale)
+  {
+    $this->locale = $locale;
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function getLevel(): int
+  {
+    return $this->level;
+  }
+
+  /**
+   * @param int $level
+   * @return Category
+   */
+  public function setLevel(int $level) {
+    $this->level = $level;
+    return $this;
+  }
 
 }
