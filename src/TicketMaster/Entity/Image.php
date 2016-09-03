ef640c801e9cff38c70a1f64584bba9ea96a94c1
Ayan Ozturk <ayanozturk@gmail.com>
2016-09-03 13:20:16 +0100

    added Image entity

--- src/TicketMaster/Entity/Image.php
+++ src/TicketMaster/Entity/Image.php
@@ -1,8 +1,125 @@
 <?php
 namespace TicketMaster\Entity;
 
-
+/**
+ * Class Image
+ * @package TicketMaster\Entity
+ */
 class Image
 {
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var string
+     */
+    private $ratio;
+
+    /**
+     * @var int
+     */
+    private $width;
+
+    /**
+     * @var int
+     */
+    private $height;
+
+    /**
+     * @var bool
+     */
+    private $fallback;
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return Image
+     */
+    public function setUrl(string $url): Image
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRatio(): string
+    {
+        return $this->ratio;
+    }
+
+    /**
+     * @param string $ratio
+     * @return Image
+     */
+    public function setRatio(string $ratio): Image
+    {
+        $this->ratio = $ratio;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    /**
+     * @param int $width
+     * @return Image
+     */
+    public function setWidth(int $width): Image
+    {
+        $this->width = $width;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param int $height
+     * @return Image
+     */
+    public function setHeight(int $height): Image
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFallback(): bool
+    {
+        return $this->fallback;
+    }
+
+    /**
+     * @param bool $fallback
+     * @return Image
+     */
+    public function setFallback(bool $fallback): Image
+    {
+        $this->fallback = $fallback;
+        return $this;
+    }
 
 }
