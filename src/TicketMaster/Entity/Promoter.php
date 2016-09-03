de273188c3ca1c25dc7519228dc129c5ec0d332a
Ayan Ozturk <ayanozturk@gmail.com>
2016-09-03 13:55:42 +0100

    added promoter entity

--- src/TicketMaster/Entity/Promoter.php
+++ src/TicketMaster/Entity/Promoter.php
@@ -8,4 +8,26 @@ namespace TicketMaster\Entity;
 class Promoter
 {
 
+    /**
+     * @var
+     */
+    private $id;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Promoter
+     */
+    public function setId(string $id): Promoter
+    {
+        $this->id = $id;
+        return $this;
+    }
 }
