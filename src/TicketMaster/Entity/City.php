faea357f5a3d8d97e98c27827a1934cf153e9665
Saphyel <Saphyel@gmail.com>
2016-09-03 13:06:39 +0100

    Add City

---
+++ src/TicketMaster/Entity/City.php
@@ -0,0 +1,33 @@
+<?php
+namespace TicketMaster\Entity;
+
+/**
+ * Class City
+ * @package TicketMaster\Entity
+ */
+class City
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return City
+     */
+    public function setName(string $name): City
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+}
